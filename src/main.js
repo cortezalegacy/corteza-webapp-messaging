b3a47b501198615c5a659f8e4b2260561b0d3c61
Denis Arh <denis.arh@kendu.si>
2018-10-14 08:55:27 +0200

    Add version & build time

--- src/main.js
+++ src/main.js
@@ -13,8 +13,13 @@ import './main.scss'
 if (window.CrustConfig === undefined) {
   alert('Unexisting or invalid configuration. Make sure there is a public/config.js configuration file.')
 } else {
-  const unsync = sync(store, router)
+  /* eslint-disable no-undef */
+  console.log(
+    `%cCrust Messaging, version: ${CRUST_VERSION}, build time: ${CRUST_BUILD_TIME}`,
+    'background-color: #1397CB; color: white; padding: 3px 10px; border: 1px solid black; font: Courier',
+  )
 
+  const unsync = sync(store, router)
   /* eslint-disable no-new */
   new Vue({
     store,
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,16 @@
+var webpack = require('webpack')
+var exec = require('child_process').execSync
+
 module.exports = {
   baseUrl: '/messaging/',
   configureWebpack: {
     // other webpack options to merge in ...
+    plugins: [
+      new webpack.DefinePlugin({
+        CRUST_VERSION: JSON.stringify(('' + exec('git describe --always --tags')).trim()),
+        CRUST_BUILD_TIME: JSON.stringify((new Date()).toISOString()),
+      }),
+    ],
   },
   // devServer Options don't belong into `configureWebpack`
   devServer: {
