56288720a038a80120d8dfd0b35911f8cf78fec2
Denis Arh <denis.arh@kendu.si>
2018-10-16 15:58:29 +0200

    Support for embedded YouTube videos (if msg contents == yt link only for now)

---
+++ src/components/Channel/EmbeddedBox.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="embedded-frame">
+    <p>
+      <a :href="chunk" v-html="chunk" />
+    </p>
+    <iframe
+      type="text/html"
+      width="100%"
+      height="265"
+      frameborder="0"
+      allowfullscreen
+      :src="src" />
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    chunk: {
+      type: String,
+      required: true,
+      default: '',
+    },
+
+    src: {
+      type: String,
+      required: true,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style scoped>
+.embedded-frame p {
+  margin: 0 0 10px 0;
+}
+
+</style>
--- src/components/Channel/HistoryMessage.vue
+++ src/components/Channel/HistoryMessage.vue
@@ -1,6 +1,7 @@
 <template>
   <span class="crust_iam_main__message__content-wrap">
     <component
+      v-if="!isEmbedded.src"
       :is="getChunkTag(c)"
       class="spaced"
       v-for="c in chunks"
@@ -8,14 +9,42 @@
       v-bind="c.props"
       v-html="c.chunk" />
 
+    <embedded-box
+      v-if="isEmbedded.src"
+      :src="isEmbedded.src"
+      :chunk="isEmbedded.chunk" />
+
   </span>
 </template>
 
 <script>
+import EmbeddedBox from './EmbeddedBox'
+
 export default {
   methods: {
     getChunkTag (chunk) {
       return chunk.meta.tag || 'span'
+    },
+
+    isYtLink (msg) {
+      if (msg) {
+        let regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=|\?v=)([^#&?]*).*/
+        let match = msg.match(regExp)
+        if (match && match[2].length === 11) {
+          return `https://www.youtube.com/embed/${match[2]}?autoplay=1&enablejsapi=1`
+        }
+        return false
+      }
+    },
+  },
+
+  computed: {
+    isEmbedded () {
+      if (this.chunks.length > 1) return false
+      let [ c = {} ] = this.chunks
+      c = c.chunk
+
+      return { src: this.isYtLink(c), chunk: c }
     },
   },
 
@@ -25,6 +54,10 @@ export default {
       required: true,
       default: () => [],
     },
+  },
+
+  components: {
+    EmbeddedBox,
   },
 }
 </script>
