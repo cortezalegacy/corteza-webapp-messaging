3c1cd6ee66f92ed55bcc4b40796804223a5e722a
Juan Non <juan@polux.studio>
2018-10-17 16:26:23 +0200

    removed all .crust-* references in users panel in favor of menu-layer

--- src/components/Panel/Users.vue
+++ src/components/Panel/Users.vue
@@ -1,16 +1,21 @@
 <template>
-  <div class="crust_iam-channel_members crust_sliding_menu" @onclick="$emit('openDirectMessage', u.ID);">
+  <div
+    class="menu-layer right"
+    :class="[
+      { display : isUserPanelOpen }
+    ]"
+    @onclick="$emit('openDirectMessage', u.ID);">
     <label class="closer"
            @click="toggleUserPanel(false)"
            aria-label="Close"><i class="icon-close"></i></label>
-    <ul class="crust_iam-channel_member_list" v-if="users">
+    <ul v-if="users">
       <li
         v-for="u in users"
         :key="u.ID"
         @click="$emit('openDirectMessage', u.ID);"
-        v-bind:class="[{active:u.connections}, 'crust_iam-channel_member']">
+        v-bind:class="[{active:u.connections}, 'channel-member']">
         <user-avatar :user="u" />
-        <span class="crust_iam-channel_member_name">{{ u | userLabel }} <i title="connected" class='icon-bubble2' v-if="u.connections"></i></span>
+        <span class="member-name">{{ u | userLabel }} <i title="connected" class='icon-bubble2' v-if="u.connections"></i></span>
       </li>
     </ul>
   </div>
@@ -28,6 +33,7 @@ export default {
   computed: {
     ...mapGetters({
       users: 'users/list',
+      isUserPanelOpen: 'ui/isUserPanelOpen',
     }),
   },
 
@@ -59,11 +65,7 @@ export default {
 <style scoped lang="scss">
   //inlude generic definitions
   @import '@/assets/sass/_0.commons.scss';
-
-.crust_sliding_menu {
-  top:0;
-  width:320px;
-}
+  @import '@/assets/sass/menu-layer.scss';
 
 .closer
 {
@@ -73,19 +75,12 @@ export default {
   font-size:20px;
 }
 
-.crust_iam-channel_member_list
-{
-  list-style : none;
-  margin:0;
-  padding:20px 0 5px 5px;
-}
-
-.crust_iam-channel_member
+.channel-member
 {
   line-height:30px;
   margin-top:10px;
 }
-.crust_iam-channel_member_name
+.member-name
 {
   display:inline-block;
   line-height:1;
@@ -97,51 +92,4 @@ li:hover {
   background:rgba($appgrey,0.15);
   border-radius:30px 0 0  30px;
 }
-
-.crust
-{
-// mainmenu switch
-&_sliding_menu
-{
-//display:none;
-position:fixed;
-width:0%;
-height:100vh;
-max-height:100vh;
-overflow-x:hidden;
-overflow-y:auto;
-background-color:$headerbgcolor;
-border-right:solid 1px $defaultlinecolor;
-margin:0;
-left:0;
-top:0;
-margin:0;
-z-index:2;
--webkit-transition: width 0.5s ease; /* For Safari 3.1 to 6.0 */
-transition: width 0.5s ease;
-}
-
-&_iam-menu
-{
-line-height:2rem;
-list-style:none;
-}
-
-&_iam-menu_item
-{
-padding:0.2rem 1.5rem;
-margin:0;
-list-style:none;
-}
-
-&_iam-channel_members
-{
-right:0;
-left:auto;
-border-right:none;
-border-left:solid 1px $defaultlinecolor;
-}
-}
-
-
 </style>
