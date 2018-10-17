3de1253290fc82d88b8624a0546a00081fb8e0e5
Juan Non <juan@polux.studio>
2018-10-17 15:49:04 +0200

    Major cleanup of useless scss files, removing almost all .crust-* main.scss trimmed to (almost) bar minimum.

--- src/components/Panel/Users.vue
+++ src/components/Panel/Users.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="crust_iam-channel_members crust_sliding_menu" @onclick="$emit('openDirectMessage', u.ID);">
-    <label class="crust_hide-iam-members crust-closer"
-           @click="toggleUserPanel(false)"><i class="icon-close"><span class="crust_readable_text">close</span></i></label>
+    <label class="closer"
+           @click="toggleUserPanel(false)"
+           aria-label="Close"><i class="icon-close"></i></label>
     <ul class="crust_iam-channel_member_list" v-if="users">
       <li
         v-for="u in users"
@@ -64,11 +65,12 @@ export default {
   width:320px;
 }
 
-.crust-closer
+.closer
 {
   position:fixed;
   top:5px;
   right:20px;
+  font-size:20px;
 }
 
 .crust_iam-channel_member_list
@@ -77,6 +79,7 @@ export default {
   margin:0;
   padding:20px 0 5px 5px;
 }
+
 .crust_iam-channel_member
 {
   line-height:30px;
@@ -94,4 +97,51 @@ li:hover {
   background:rgba($appgrey,0.15);
   border-radius:30px 0 0  30px;
 }
+
+.crust
+{
+// mainmenu switch
+&_sliding_menu
+{
+//display:none;
+position:fixed;
+width:0%;
+height:100vh;
+max-height:100vh;
+overflow-x:hidden;
+overflow-y:auto;
+background-color:$headerbgcolor;
+border-right:solid 1px $defaultlinecolor;
+margin:0;
+left:0;
+top:0;
+margin:0;
+z-index:2;
+-webkit-transition: width 0.5s ease; /* For Safari 3.1 to 6.0 */
+transition: width 0.5s ease;
+}
+
+&_iam-menu
+{
+line-height:2rem;
+list-style:none;
+}
+
+&_iam-menu_item
+{
+padding:0.2rem 1.5rem;
+margin:0;
+list-style:none;
+}
+
+&_iam-channel_members
+{
+right:0;
+left:auto;
+border-right:none;
+border-left:solid 1px $defaultlinecolor;
+}
+}
+
+
 </style>
