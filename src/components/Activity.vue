b0542153a3f74946c59ae74306e076d24d0951f1
Denis Arh <denis.arh@kendu.si>
2018-10-25 17:21:56 +0200

    Improve activity (who's typing)

--- src/components/Activity.vue
+++ src/components/Activity.vue
@@ -1,9 +1,10 @@
 <!-- template for the user avatar component -->
 <template>
   <section v-if="users.length > 0" class="activity">
-    <b v-for="(user, i) in users" :key="user.ID" class="user">{{ i > 0 ? ', ' : '' }}{{user | userLabel}}</b>
-    <span v-if="users.length == 1"> is </span>
-    <span v-else> are </span>
+    <b v-for="(user, i) in users.slice(0, limit)" :key="user.ID" class="user">{{ i > 0 ? ', ' : '' }}{{user | userLabel}}</b>
+    <span v-if="users.length > limit"> and {{users.length - limit}} others ... are </span>
+    <span v-else-if="users.length > 1"> are </span>
+    <span v-else> is </span>
     <slot></slot>
   </section>
 </template>
@@ -12,8 +13,16 @@
 export default {
   // require user param
   props: {
-    users: Array,
-    required: true,
+    users: {
+      type: Array,
+      required: true,
+    },
+
+    limit: {
+      type: Number,
+      required: false,
+      default: 4,
+    },
   },
 }
 </script>
