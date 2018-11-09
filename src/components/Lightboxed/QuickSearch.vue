60b824fa6dc3146c002faa512bae3f7a759772ef
Mia Arh <mia@kendu.si>
2018-11-09 09:04:08 +0100

    Style quick search lightbox

--- src/components/Lightboxed/QuickSearch.vue
+++ src/components/Lightboxed/QuickSearch.vue
@@ -3,9 +3,13 @@
       <header>
           <span class="closer" @click="onClose">&times;</span>
       </header>
-      <main>
-          <search-input focus="true" v-model="query" placeholder="Find channel, group or user..."  />
+      <main class="quick-search">
+          <search-input focus="true" v-model="query"
+          placeholder="Find channel, group or user..."  />
           <ol>
+              <li v-if="query && filtered.length===0" class="no-results">
+                No matches found
+              </li>
               <li v-for="c in (query ? filtered : prefered).slice(0, 10)"
                   @click="onClose"
                   :key="c.ID">
@@ -75,33 +79,33 @@ header {
   height: 50px;
   width: 50vw;
 
-  section {
-    padding: 10px 70px 0 15px;
-  }
-
   .closer {
     position: fixed;
     color: $appcream;
     float: right;
     font-size: 50px;
-    font-weight: bold;
     top: 10px;
     right: 30px;
     cursor: pointer;
     line-height: 30px;
   }
 }
-
+form{
+  padding-bottom: 3px;
+}
 main {
-  height: 40vh;
-  width: 50vw;
+  max-height: 70vh;
+  width: 90vw;
   background-color: $appwhite;
-  margin: 15vh 0 0 25vw;
+  margin: 5vh 0 0 5vw;
+  padding: 20px 20px 3px;
 
   ol {
     list-style-type: none;
     margin: 0;
     padding: 0;
+    max-height: 60vh;
+    overflow: scroll;
 
     li {
       margin: 0;
@@ -120,12 +124,31 @@ main {
 }
 
 footer {
-  width: 50vw;
+  text-align: center;
+  width: 90vw;
   background-color: $appcream;
-  margin: 0 0 0 25vw;
-  color: $appgrey;
-  padding: 5px;
+  border-top: 2px solid $appgrey;
+  margin: 0 0 0 5vw;
+  padding: 10px;
+  position: fixed;
+}
 
+.no-results{
+  text-align: center;
+  font-size: 18px;
+  padding: 20px;
+}
+
+@media (min-width: $confortableminwidth)
+{
+  footer{
+    max-width: 50vw;
+    margin: 0 0 0 25vw;
+  }
+  main{
+    max-width: 50vw;
+    margin: 15vh 0 0 25vw;
+  }
 }
 
 </style>
