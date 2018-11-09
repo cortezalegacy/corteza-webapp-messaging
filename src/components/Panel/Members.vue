aaa1dfc5801607645b976315eea285d99cbcb822
Mia Arh <mia@kendu.si>
2018-11-09 09:41:34 +0100

    Style panel member list

--- src/components/Panel/Members.vue
+++ src/components/Panel/Members.vue
@@ -2,16 +2,21 @@
   <base-panel
     v-on="$listeners"
     @onclick="$emit('openDirectMessage', u.ID);">
-    <template slot="header">Users</template>
-    <template slot="subtitle">in <channel-name :channel="channel"></channel-name></template>
+    <template slot="header">Members</template>
+    <template slot="subtitle">in
+      <channel-name :channel="channel"></channel-name>
+    </template>
     <template slot="main">
       <ul v-if="members">
         <li
           v-for="u in members"
           :key="u.ID"
-          @click="$emit('openDirectMessage', u.ID);">
+          @click="$emit('openDirectMessage', u.ID);"
+          class="channel-member"
+          >
           <user-avatar :user="u" />
-          <span class="member-name">{{ u | userLabel }} <i title="connected" class='icon-bubble2' v-if="u.connections"></i></span>
+          <span class="member-name">{{ u | userLabel }}</span>
+          <i title="connected" class='message-icon icon-bubble2' v-if="u.connections"></i>
         </li>
       </ul>
     </template>
@@ -63,12 +68,16 @@ export default {
   right:20px;
   font-size:20px;
 }
-
+.channel-name{
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
 .channel-member
 {
   line-height:30px;
   margin-top:10px;
   margin-left:20px;
+  cursor: pointer;
 }
 .member-name
 {
@@ -76,7 +85,14 @@ export default {
   line-height:1;
   max-width:80%;
   overflow:hidden;
-  padding-left:5px;
+  padding-left:10px;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+.message-icon{
+  float: right;
+  margin-right: 20px;
+  line-height: 30px;
 }
 li:hover {
   background:rgba($appgrey,0.15);
--- src/components/Panel/index.vue
+++ src/components/Panel/index.vue
@@ -51,7 +51,6 @@ header {
 }
 
 main {
-
 }
 
 </style>
