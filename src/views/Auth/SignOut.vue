6b7624fd62ccfaad1b29fc2fe458d508cb5b5c17
Denis Arh <denis.arh@kendu.si>
2018-11-19 07:02:17 +0100

    No need for template/styles for sign-out

--- src/views/Auth/SignOut.vue
+++ src/views/Auth/SignOut.vue
@@ -1,18 +1,3 @@
-<template>
-  <div class="signIn login-process centered">
-    <div class="app-form-wrap shaded">
-      <div class="crust-window-header">
-        <div class="crust_main-header_title-wrap">
-          <strong class="crust_main-header_title">Crust</strong>
-          <span class="crust_main-header_title__pf">platform</span>
-        </div>
-      </div>
-      <form class="large-form">
-        Signing out...
-      </form>
-    </div>
-  </div>
-</template>
 <script>
 export default {
   data () {
