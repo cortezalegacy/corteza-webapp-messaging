dccd5372a3c10ea3a984a4e52167305ceffe89f5
Denis Arh <denis.arh@kendu.si>
2019-02-15 07:22:14 +0100

    Improve sign-in/out

--- src/views/Auth/SignOut.vue
+++ src/views/Auth/SignOut.vue
@@ -1,10 +1,19 @@
 <script>
+import axios from 'axios'
+
 export default {
+  render () { return null },
   beforeCreate () {
+    const { adtSignOutUrl } = window.CrustConfig.webapp.auth || {}
+    if (adtSignOutUrl) {
+      axios({ method: 'GET', withCredentials: true, url: adtSignOutUrl })
+    }
+
     this.$system.authLogout().catch((err) => {
-      console.error(err)
+      this.$logger.error(err)
     }).finally(() => {
       this.$store.commit('auth/clean')
+      this.$store.commit('auth/loaded', true)
     })
     this.$router.push({ name: 'signin' })
   },
