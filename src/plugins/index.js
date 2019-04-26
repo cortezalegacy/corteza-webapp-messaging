61a6020ea11ec503f175db917fd353f1cd283edd
Tomaž Jerman <tomaz.jerman@kendu.si>
2019-04-26 16:22:51 +0200

    Update for new configs

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -7,6 +7,7 @@ import commands from '@/plugins/commands'
 import triggers from '@/plugins/triggers'
 import logger from '@/plugins/logger'
 import messaging from '@/plugins/messaging'
+import auth from 'crust-common.vue/src/plugins/auth'
 
 const eventbus = new Vue()
 
@@ -16,6 +17,7 @@ Vue.use(bus, { eventbus })
 Vue.use(system)
 Vue.use(commands, { store })
 Vue.use(ws, { eventbus })
+Vue.use(auth)
 Vue.use(triggers, {
   userByID: store.getters['users/findByID'],
   userList: store.getters['users/listOnDemand'],
