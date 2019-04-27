d8fec5a3a4038de72db27784abba3d83feeb6dcd
Tomaž Jerman <tomaz.jerman@kendu.si>
2019-04-27 21:05:08 +0200

    Use currentUser from prop

--- src/components/Messages/Message/index.vue
+++ src/components/Messages/Message/index.vue
@@ -58,7 +58,7 @@
         </section>
         <div
           class="selectable"
-          :class="{ from_me: (message.user || {}).ID === $auth.user.ID,
+          :class="{ from_me: (message.user || {}).ID === currentUser.ID,
           'message' : !inEditing,
            }">
           <attachment
