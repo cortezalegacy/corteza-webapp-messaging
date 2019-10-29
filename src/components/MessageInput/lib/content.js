81aaaed70bbcfb0385ddaea6803cfd97445e9a94
Tomaž Jerman <tomaz.jerman@kendu.si>
2019-10-29 19:24:25 +0100

    Add document stringify/parse helpers

--- src/components/MessageInput/lib/content.js
+++ src/components/MessageInput/lib/content.js
@@ -1,4 +1,4 @@
-// Provides content related helpers, such as is content empty.
+// Provides content related helpers, such as content stringification, parsing, ...
 
 /**
  * Helper to determine if given content is empty or not.
@@ -21,4 +21,106 @@ export function getDraft (draft) {
   }
 
   return draft
+}
+
+// Mention regexps
+export const mentionSplitRE = new RegExp(`(<[@#]\\d+\\s?[^>]*?>)`)
+export const mentionRE = new RegExp(`<([@#])(\\d+)((?:\\s)([^>]+))?>`)
+
+/**
+ * Helper function to export given document to plain text
+ * @param {Document} doc Document object, representing current message
+ * @returns {String}
+ */
+export function stringifyDocument (doc = {}) {
+  let out = ``
+  for (const c of doc.content || []) {
+    if (c.type === 'paragraph') {
+      out += stringifyLine(c) + '\n'
+    }
+  }
+
+  return out.trim()
+}
+
+/**
+ * Helper function to stringify the given Document. Handles mentions.
+ * @param {Array} content Doc's content nodes
+ * @returns {String}
+ * @private
+ */
+function stringifyLine ({ content = [] }) {
+  let line = ``
+
+  for (const { type, ...rest } of content) {
+    if (type === 'text') {
+      line += rest.text
+    } else {
+      line += `<${type.split('-').pop()}${rest.attrs.id}>`
+    }
+  }
+
+  return line.trim()
+}
+
+/**
+ * Helper function to parse a given string to the Document object
+ * @param {String} message Message to parse
+ * @returns {Document}
+ */
+export function parseDocument (message = '') {
+  let lines = message
+    .trim()
+    .split('\n')
+
+  const content = []
+  for (const line of lines) {
+    const cc = parseLine(line)
+    // If content is empty, it can be omitted
+    if (cc.length) {
+      content.push({
+        type: 'paragraph',
+        content: cc,
+      })
+    } else {
+      content.push({
+        type: 'paragraph',
+      })
+    }
+  }
+  return {
+    type: 'doc',
+    content,
+  }
+}
+
+/**
+ * Helper function to parse a given line to a content node
+ * @param {String} line Line
+ * @returns {Array}
+ * @private
+ */
+function parseLine (line = '') {
+  const content = []
+  const chunks = line.split(mentionSplitRE, -1)
+
+  for (let i = 0; i < chunks.length; i++) {
+    // Since we are splitting, odd chunks represent plain text
+    if (i % 2 !== 1) {
+      if (chunks[i]) {
+        content.push({ type: 'text', text: chunks[i] })
+      }
+    } else {
+      const [ , denotationChar, id, , label ] = mentionRE.exec(chunks[i])
+      content.push({
+        type: `mention-${denotationChar}`,
+        attrs: {
+          id,
+          label,
+        },
+      })
+    }
+  }
+
+  return content
 }
--- tests/unit/specs/components/MessageInput/lib.spec.js
+++ tests/unit/specs/components/MessageInput/lib.spec.js
@@ -2,7 +2,11 @@
 /* ESLint didn't like some expects */
 
 import { expect } from 'chai'
-import { getDraft } from 'corteza-webapp-messaging/src/components/MessageInput/lib'
+import {
+  getDraft,
+  stringifyDocument,
+  parseDocument,
+} from 'corteza-webapp-messaging/src/components/MessageInput/lib'
 
 describe('src/components/MessageInput/lib', () => {
   describe('getDraft', () => {
@@ -17,6 +21,148 @@ describe('src/components/MessageInput/lib', () => {
       for (const test of tests) {
         const d = getDraft(test.draft)
         expect(d, test.name).to.deep.eq(test.expected)
+      }
+    })
+  })
+
+  describe('stringifyDocument', () => {
+    it('handle stringification', () => {
+      const tests = [
+        {
+          name: 'plain text',
+          doc: {
+            content: [
+              { type: 'paragraph', content: [ { type: 'text', text: 'l1' } ] },
+              { type: 'paragraph', content: [ { type: 'text', text: 'l2' } ] },
+            ]
+          },
+          expected: `l1\nl2`,
+        },
+
+        {
+          name: 'handle empty lines; trim trailing lines',
+          doc: {
+            content: [
+              { type: 'paragraph', content: [ { type: 'text', text: 'l1' } ] },
+              { type: 'paragraph' },
+              { type: 'paragraph', content: [ { type: 'text', text: 'l2' } ] },
+              { type: 'paragraph' },
+              { type: 'paragraph' },
+            ]
+          },
+          expected: `l1\n\nl2`,
+        },
+
+        {
+          name: 'user mentions',
+          doc: {
+            content: [
+              {
+                type: 'paragraph',
+                content: [
+                  { type: 'text', text: 'text ' },
+                  { type: 'mention-@', attrs: { id: '111', label: 'Mention User' } },
+                  { type: 'text', text: ' text' },
+                ],
+              },
+            ]
+          },
+          expected: `text <@111> text`,
+        },
+
+        {
+          name: 'channel mentions',
+          doc: {
+            content: [
+              {
+                type: 'paragraph',
+                content: [
+                  { type: 'text', text: 'text ' },
+                  { type: 'mention-#', attrs: { id: '222', label: 'Mention Channel' } },
+                  { type: 'text', text: ' text' },
+                ],
+              },
+            ]
+          },
+          expected: `text <#222> text`,
+        },
+      ]
+
+      for (const t of tests) {
+        const ss = stringifyDocument(t.doc)
+        expect(ss, t.name).to.eq(t.expected)
+      }
+    })
+  })
+
+  describe('parseDocument', () => {
+    it('handle parsing', () => {
+      const tests = [
+        {
+          name: 'plain text',
+          message: `l1\nl2`,
+          expected: {
+            type: 'doc',
+            content: [
+              { type: 'paragraph', content: [ { type: 'text', text: 'l1' } ] },
+              { type: 'paragraph', content: [ { type: 'text', text: 'l2' } ] },
+            ]
+          },
+        },
+
+        {
+          name: 'handle empty lines; trim trailing lines',
+          message: `l1\n\nl2`,
+          expected: {
+            type: 'doc',
+            content: [
+              { type: 'paragraph', content: [ { type: 'text', text: 'l1' } ] },
+              { type: 'paragraph' },
+              { type: 'paragraph', content: [ { type: 'text', text: 'l2' } ] },
+            ]
+          },
+        },
+
+        {
+          name: 'user mentions',
+          message: `text <@111 Mention User> text`,
+          expected: {
+            type: 'doc',
+            content: [
+              {
+                type: 'paragraph',
+                content: [
+                  { type: 'text', text: 'text ' },
+                  { type: 'mention-@', attrs: { id: '111', label: 'Mention User' } },
+                  { type: 'text', text: ' text' },
+                ],
+              },
+            ]
+          },
+        },
+
+        {
+          name: 'channel mentions',
+          message: `text <#222 Mention Channel> text`,
+          expected: {
+            type: 'doc',
+            content: [
+              {
+                type: 'paragraph',
+                content: [
+                  { type: 'text', text: 'text ' },
+                  { type: 'mention-#', attrs: { id: '222', label: 'Mention Channel' } },
+                  { type: 'text', text: ' text' },
+                ],
+              },
+            ]
+          },
+        },
+      ]
+
+      for (const t of tests) {
+        const ss = parseDocument(t.message)
+        expect(ss, t.name).to.deep.eq(t.expected)
       }
     })
   })
