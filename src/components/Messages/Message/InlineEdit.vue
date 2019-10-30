3f017235f84473482d130dbe10625d5ae21509f3
Tomaž Jerman <tomaz.jerman@kendu.si>
2019-10-30 22:06:56 +0100

    Define inline message edit component

---
+++ src/components/Messages/Message/InlineEdit.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="editing">
+    <message-input
+      ref="input"
+      v-model="draft"
+      :channel="channel"
+      :focus="focus"
+      :submit-on-enter="submitOnEnter"
+      :user-suggestions="userSuggestions"
+      :channel-suggestions="channelSuggestions"
+      :current-user="currentUser"
+      :source="channelID"
+      @submit="onSubmit"
+      @focus="onFocus"
+      v-on="$listeners"
+    >
+      <template slot="sectionRight">
+        <button
+          v-if="!hideEmojiButton"
+          class="emoji-button input-button"
+          @click.stop="onEmojiClick"
+        >
+          <span class="icon-smile" />
+        </button>
+
+        <button
+          v-if="!submitOnEnter"
+          class="input-button send-button"
+          :disabled="submitDisabled"
+          @click="onSubmit"
+          @mousedown="eventSink"
+        >
+          <span class="icon-hsend" />
+        </button>
+      </template>
+    </message-input>
+  </div>
+</template>
+
+<script>
+import { stringifyDocument, parseDocument } from 'corteza-webapp-messaging/src/components/MessageInput/lib'
+import base from 'corteza-webapp-messaging/src/components/Chat/Footer/base'
+
+/**
+ * Component handeles inline message editing, updating & deletitions.
+ */
+export default {
+  extends: base,
+
+  props: {
+    message: {
+      type: Object,
+      required: true,
+      default: () => ({}),
+    },
+  },
+
+  data () {
+    return {
+      draft: parseDocument(this.message.message),
+    }
+  },
+
+  computed: {
+    messageID () {
+      return this.message.messageID
+    },
+  },
+
+  methods: {
+    /**
+     * Handle input's submit event. Cases:
+     *  - if content is empty: prompt message delete
+     *  - else: edit the given message
+     */
+    onSubmit () {
+      const message = stringifyDocument(this.draft)
+      console.log('x', { message })
+      const stdResponse = (m) => {
+        this.draft = null
+        this.$emit('cancel')
+      }
+
+      if (!message) {
+        this.$emit('deleteMessage')
+      } else if (this.channel) {
+        this.$MessagingAPI.messageEdit({ ...this.message, message })
+          .then(stdResponse)
+      }
+    },
+  },
+}
+</script>
+<style lang="scss" scoped>
+@import 'corteza-webapp-messaging/src/components/Chat/Footer/common.scss';
+
+</style>
