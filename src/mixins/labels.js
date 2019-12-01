04c67f73e8b7519fb5ee53e352d0ab97d8aa0131
Tomaž Jerman <tomaz.jerman@kendu.si>
2019-12-01 23:31:51 +0100

    Cleanup mixins/labels

--- src/mixins/labels.js
+++ src/mixins/labels.js
@@ -2,7 +2,7 @@ import { User, Channel } from 'corteza-webapp-messaging/src/types'
 
 export default {
   methods: {
-    labelChannel (c) {
+    labelChannel (c, users = {}) {
       if (!c) {
         console.error('Invalid channel label param %o', c)
       }
@@ -18,32 +18,18 @@ export default {
       if (c.type === 'group') {
         return this.$store.getters['channels/otherMembersOf'](c.channelID, this.$auth.user.userID)
           // Make sure user exists
-          .filter(o => this.$store.getters['users/findByID'](o) !== undefined)
-          .map(this.labelUser).join(', ')
+          .filter(o => users[o] !== undefined)
+          .map(u => u.label)
+          .join(', ')
       }
 
       return c.name || `<#${c.channelID}>`
     },
 
-    labelUser (u) {
-      if (!u) {
-        console.error('Invalid user label param %o', u)
-      }
-
-      if (typeof u === 'string') {
-        u = this.$store.getters['users/findByID'](u)
-      }
-
-      if (!u) {
-        return this.$t('general.label.na')
-      }
-
-      return u.name || u.username || u.handle || u.email || `<@${u.userID}>`
-    },
-
     getLabel (o) {
       if (o instanceof User) {
-        return this.labelUser(o)
+        // For legacy reasons; should be removed at some point
+        return o.label
       }
 
       if (o instanceof Channel) {
